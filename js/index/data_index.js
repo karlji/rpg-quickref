ed1e4a37bd6bfd1735b988628ea044eb66e5a3b2
Jiri Karlik <karlikjirka@gmail.com>
2025-09-09 12:41:11 +0200

    DnD renamed to contain edition number. PF2 added to the index.

--- js/index/data_index.js
+++ js/index/data_index.js
@@ -1,8 +1,14 @@
 data_index = [
     {
-        title: "DnD 5e",
+        title: "DnD5e",
         icon: "dungeon-gate",
         subtitle: "Dungeons and Dragons 5th Edition",
-        url: "/dnd.html"
-    }
+        url: "/dnd5.html"
+    },
+    {
+    title: "PF2e",
+    icon: "compass",
+    subtitle: "Pathfinder 2nd Edition",
+    url: "/pf2.html"
+    },
 ]
